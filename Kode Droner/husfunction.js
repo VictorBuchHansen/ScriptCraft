7533825bfbbeedd1666cec8f97607a07bcb77fd9
VictorBuchHansen <Victorbuchhansen@gmail.com>
2017-03-19 20:06:35 +0100

    Update husfunction.js

--- Kode Droner/husfunction.js
+++ Kode Droner/husfunction.js
@@ -1,8 +1,23 @@
-function husfunction(a) {
+function husfunction(a,loegde,hoede,dybde) {
 	var vaeg='5'
 	var tag='1'
-	if(a == 'default'){
+
+	if (typeof a == 'undefined' ) {
+		a = 'default'
 	}
+	if (typeof loegde == 'undefined' ) {
+		loegde = 7
+	}
+	if (typeof hoede == 'undefined' ) {
+		hoede = 4
+	}
+	if (typeof dybde == 'undefined' ) {
+		dybde = 7
+	}
+
+	if(a == 'default'){
+
+}
  else if (a =='pink') {
 	 tag='41'
 	 vaeg='35:6'
@@ -10,37 +25,70 @@ function husfunction(a) {
 	 else if (a =='blue') {
 	 	 vaeg='35:3'
 		 tag='22'
-	 }
+}
 	 else if (a =='green') {
 		 vaeg='35:5'
 		 tag='35:4'
-
-	 }
+}
 	 else if (a =='tnt') {
 		 vaeg='46'
 		 tag='46'
-	 }
- this.box0(vaeg,7,4,7);
- this.up(4);
- this.prism0(tag,7,7);
- this.prism(tag,0,7);
- this.prism(tag,1,6,4);
- this.right(6);
- this.prism(tag,1,6,4);
- this.down(4);
- this.left(5);
- this.box('0',1,2,1);
- this.up(1);
- this.right(2);
- this.box('102',1,1,1);
- this.right(2);
- this.box('102',1,1,1);
- this.fwd(5);
- this.down(1);
- this.box('58',1,1,1);
- this.left(3);
- this.up(1);
- this.hangtorch(1);
+ }
+	 else if (a =='diamond') {
+		vaeg='57'
+		tag='57'
+}
+	else if (a =='lava') {
+		vaeg='11'
+		tag='11'
+}
+	if (a =='vand') {
+		vaeg='9'
+		tag='9'
+}
+
+
+	this.chkpt('houseStart');
+  this.up(hoede);
+  this.prism(tag, loegde,dybde);
+  this.move('houseStart');
+  this.right(Math.floor(loegde/2));
+  this.box(0,1,2,1);
+  this.move('houseStart');
+  this.right(loegde)
+
+
+
+/*this.box0(vaeg,loegde,hoede,dybde);
+this.up(4);
+this.prism0(tag,loegde,dybde);
+this.prism(tag,loegde,dybde);
+this.right(6);
+this.prism(tag,1,dybde);
+this.down(4);
+this.left(5);
+this.box('0',1,2,1);
+this.up(1);
+this.right(2);
+this.box('102',1,1,1);
+this.right(2);
+this.box('102',1,1,1);
+this.fwd(5);
+this.down(1);
+*/
+
+if(a!='lava')
+{
+this.box('58',1,1,1);
+}
+
+if(a!='vand' && a!='lava')
+{
+this.box('58',1,1,1);
+this.left(3);
+this.up(1);
+this.hangtorch(1);
+}
 
 };
 
